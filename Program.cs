7af51e63130ee214522e5c7d23bd303e93bf5039
Gabriel Serpa <gabrielmadrugaserpa08@gmail.com>
2024-10-25 11:52:15 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+public class PrimeiroPrograma
+{
+
+    public static void Main(string[] args)
+    {
+        /*
+        int valor1 = 8;
+        int valor2 = 3;
+       
+        
+        // cálculos
+        int soma = valor1 + valor2;
+        int substracao = valor1 - valor2;
+        int multiplicacao = valor1 * valor2;
+        int divisao = valor1 / valor2;
+        int resto = valor1 % valor2;
+
+        // resultado
+        Console.WriteLine("A Soma é igual a " + soma);
+        Console.WriteLine("subtracao = {0}",
+            substracao);
+        Console.WriteLine("multiplicacao = " +
+            multiplicacao);
+        Console.WriteLine("Divisao = {0}", divisao);
+        Console.WriteLine("Resto = " + resto);*/
+
+        bool nome = true;
+
+        if (nome)
+        {
+            Console.WriteLine("Pode entrar");
+        }
+        else
+        { 
+             Console.WriteLine()
+        } 
+
+    }
+        
+
+}
